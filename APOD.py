4142033645dfcf22f42f94cf0c27d4d559b40599
Jayden <taylorje2@etsu.edu>
2026-02-28 16:40:56 -0500

    APOD.py: Added comments/descriptions for the attributes and added the result layout

--- APOD.py
+++ APOD.py
@@ -1,9 +1,16 @@
 from pydantic import BaseModel
 
+# class for the APOD results
 class nasa_apod(BaseModel):
+    # birthdate for the APOD
     date: str
+    # explanation of the APOD
     explanation: str
+    # title of the APOD
     title: str
+    # high res version image URL of the APOD
     hdurl: str
 
-
+    # results layout of the APOD
+    def __str__(self):
+        return f"Title: {self.title}\nDate: {self.date}\nExplanation: {self.explanation}\nHD URL: {self.hdurl}"
