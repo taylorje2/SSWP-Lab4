899178db3250b290ea6552933729cabe5bba77c0
Jayden <taylorje2@etsu.edu>
2026-02-28 18:17:01 -0500

    README: updated issue and solution logs.
    main.py:
    added dotenv, params, and main program features

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Repo for CSCI 2910 - Lab 4 - Using Web APIs
 **Issue:** Pydantic has squiggly lines under it
 **Solution:** installed pydantic
 
+**Issue:** Program not running. Forgot to add query parameters for APOD API in main.py. 
+**Solution:** Added params provided by the NASA API site: https://api.nasa.gov/
 
+**Issue:** Program not running. Forgot to add dotenv to main.py
+**Solution:** Added dotenv to main.py
--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import requests
 from APOD import nasa_apod
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+NASA_API_KEY = os.getenv("NASA_API_KEY")
 
 BASE_URL = "https://api.nasa.gov/planetary/apod"
+
+while True:
+
+    date = input("Enter a birth date after 1995-06-16 (YYYY-MM-DD) or 'q' to quit: ")
+
+    if date == 'q':
+
+        break
+
+    else:
+        params = {
+            "api_key": NASA_API_KEY,
+            "date": date
+        }
+        
+        apod_result = requests.get(BASE_URL, params=params).json()
+
+        apod = nasa_apod(**apod_result)
+
+        print(apod)
 
