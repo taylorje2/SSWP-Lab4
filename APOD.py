10b9521699b31f0352de6158bbe2f617787a69bb
Jayden <taylorje2@etsu.edu>
2026-02-28 18:03:35 -0500

    APOD.py: updated attributes and result layout to include a standard image url

--- APOD.py
+++ APOD.py
@@ -8,9 +8,11 @@ class nasa_apod(BaseModel):
     explanation: str
     # title of the APOD
     title: str
+    # standard resolution image URL of the APOD
+    url: str
     # high res version image URL of the APOD
     hdurl: str
 
     # results layout of the APOD
     def __str__(self):
-        return f"Title: {self.title}\nDate: {self.date}\nExplanation: {self.explanation}\nHD URL: {self.hdurl}"
+        return f"Title: {self.title}\nDate: {self.date}\nExplanation: {self.explanation}\nURL: {self.url}\nHD URL: {self.hdurl}"
